eed1187d45e7d8a6e06a73fc2a1450aa0a71fe7f
Sheharyar <105873687+Shery-1508@users.noreply.github.com>
2023-04-22 09:57:30 -0700

    Update createjson.py
    new code

--- test_03/createjson.py
+++ test_03/createjson.py
@@ -1,56 +1,21 @@
-# import os
-# import json
-# from PIL import Image
+# Instructions
+# To install the PIL module, run "pip install PIL" in the terminal.
 
-# # Path to the models directory
-# directory = "F:\VS CODE\MyProjects\web dev\model aggragate site/tste"
-
-# # Empty dictionary to store the models and galleries
-# model_dict = {"models": []}
-
-# # Thumbnail size
-# thumb_size = (300, 300)
-
-# # Loop through each model directory
-# for model in os.listdir(directory):
-#     model_path = os.path.join(directory, model)
-#     if os.path.isdir(model_path):
-#         # Create a dictionary for the current model
-#         model_dict_curr = {"name": model, "galleries": []}
-#         # Loop through each gallery directory in the current model directory
-#         for gallery in os.listdir(model_path):
-#             gallery_path = os.path.join(model_path, gallery)
-#             if os.path.isdir(gallery_path):
-#                 # Create a dictionary for the current gallery
-#                 gallery_dict_curr = {"name": gallery, "pics": []}
-#                 # Loop through each picture file in the current gallery directory
-#                 for pic in os.listdir(gallery_path):
-#                     if pic.endswith(".jpg") or pic.endswith(".jpeg") or pic.endswith(".png"):
-#                         pic_path = os.path.join(gallery_path, pic)
-#                         thumb_path = os.path.join(gallery_path, "thumb_" + pic)
-#                         # Create a thumbnail for the current picture
-#                         with Image.open(pic_path) as im:
-#                             im.thumbnail(thumb_size)
-#                             im.save(thumb_path)
-#                         # Create a dictionary for the current picture
-#                         pic_dict_curr = {"src": pic_path, "thumb": thumb_path}
-#                         # Append the current picture dictionary to the current gallery's "pics" list
-#                         gallery_dict_curr["pics"].append(pic_dict_curr)
-#                 # Append the current gallery dictionary to the current model's "galleries" list
-#                 model_dict_curr["galleries"].append(gallery_dict_curr)
-#         # Append the current model dictionary to the "models" list in the main dictionary
-
-
-
+#    Hint for users: To edit the paths in the JSON file, select the part of the path that 
+#      needs to be updated and press "Ctrl+L" in VS Code to select the entire line.
 
 import os
 import json
+from PIL import Image
 
 # Path to the models directory
-directory = "F:\VS CODE\MyProjects\web dev\model aggragate site/tste"
+directory = r"F:\VS CODE\MyProjects\web dev\model aggragate site\tste\New folder"
 
 # Empty dictionary to store the models and galleries
 model_dict = {"models": []}
+
+# Thumbnail size
+thumb_size = (250, 250)
 
 # Loop through each model directory
 for model in os.listdir(directory):
@@ -69,6 +34,19 @@ for model in os.listdir(directory):
                     if pic.endswith(".jpg") or pic.endswith(".jpeg") or pic.endswith(".png"):
                         pic_path = os.path.join(gallery_path, pic)
                         thumb_path = os.path.join(gallery_path, "thumb_" + pic)
+                        
+                        # Uncomment the following block of code if you need to create missing thumbnails for images
+                        """
+                        # Check if thumbnail for the current picture already exists, else create one
+                        if not os.path.isfile(thumb_path):
+                            try:
+                                with Image.open(pic_path) as im:
+                                    im.thumbnail(thumb_size)
+                                    im.save(thumb_path)
+                            except Exception as e:
+                                print(f"Error creating thumbnail for {pic_path}: {e}")
+                        """
+                        
                         # Create a dictionary for the current picture
                         pic_dict_curr = {"src": pic_path, "thumb": thumb_path}
                         # Append the current picture dictionary to the current gallery's "pics" list
@@ -78,10 +56,9 @@ for model in os.listdir(directory):
         # Append the current model dictionary to the "models" list in the main dictionary
         model_dict["models"].append(model_dict_curr)
 
+# Print the final dictionary
 print(model_dict)
+
 # Write the dictionary to a JSON file
 with open("models.json", "w") as json_file:
     json.dump(model_dict, json_file)
-
-# with open("F:\FDM Downloads\PS\Photoshop_portable_CS6_x64_Multi\Photoshop_portable_CS6_x64_Multi/tste/models.json", "w") as json_file:
-#     json.dump(model_dict, json_file)
